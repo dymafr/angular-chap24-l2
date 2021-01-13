87e90b7e35402177730ce865902a6227fb15d28c
dymafr <erwan1309@hotmail.com>
2021-01-13 13:28:17 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,14 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent implements OnInit {
+  public chips: string[] = ["chip 1", "chip 2", "chip 3"];
+
   toggle(event) {
     console.log(event);
+  }
+
+  removeChip(event, chip) {
+    this.chips = this.chips.filter(el => el !== chip);
   }
 
   ngOnInit() {}
