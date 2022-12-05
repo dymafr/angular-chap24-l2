507df8133927dbd7be7b21bcf426aab97625a67b
dymafr <32801166+dymafr@users.noreply.github.com>
2022-12-05 12:46:44 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  public chips: string[] = ["chip 1", "chip 2", "chip 3"];
+  public chips: string[] = ['chip 1', 'chip 2', 'chip 3'];
 
-  toggle(event) {
+  toggle(event: any) {
     console.log(event);
   }
 
   removeChip(event, chip: string) {
-    this.chips = this.chips.filter(el => el !== chip);
+    this.chips = this.chips.filter((el) => el !== chip);
   }
 
   ngOnInit() {}
